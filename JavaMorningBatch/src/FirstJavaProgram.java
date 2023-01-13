e6311dff5f6ae3f410a747da1c4b6790efb64aeb
SMANTA\smans <smanta.shrestha@gmail.com>
2023-01-13 12:48:16 -0500

    Print hello world

---
+++ JavaMorningBatch/src/FirstJavaProgram.java
@@ -0,0 +1,9 @@
+public class FirstJavaProgram 
+{
+
+	public static void main(String args[]) // main  method
+	{
+		System.out.println("Hello world..welcome to Java training...");
+		System.out.println("Selenium training...");
+	}
+}
